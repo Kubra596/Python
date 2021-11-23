579d977a3cb538a2622e05201b4f9058a112271b
Kubra596 <kubrakarabuga75@gmail.com>
2021-11-24 00:12:19 +0300

    Add files via upload

---
+++ 3_basari_notu.py
@@ -0,0 +1,23 @@
+vize1=int(input("1. vize notunuzu giriniz: "))
+vize2=int(input("1. vize notunuzu giriniz: "))
+final=int(input("Final notunuzu giriniz: "))
+basarinotu=vize1*0.3+vize2*0.3+final*0.4
+
+if basarinotu>=90:
+    print("AA")
+elif basarinotu>=85:
+    print("BA")
+elif basarinotu>=80:
+    print("BB")
+elif basarinotu>=75:
+    print("CB")
+elif basarinotu>=70:
+    print("CC")
+elif basarinotu>=65:
+    print("DC")
+elif basarinotu>=60:
+    print("DD")
+elif basarinotu>=55:
+    print("FD")
+elif basarinotu>=50:
+    print("FF")
